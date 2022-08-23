ea1c111c254a3e61da5801f379b5866490ca95ca
송민진 <m_6595@naver.com>
2022-08-24 03:46:19 +0900

    로그인 구현 완료

--- src/main/java/com/innovation/myownblog/security/WebSecurityConfig.java
+++ src/main/java/com/innovation/myownblog/security/WebSecurityConfig.java
@@ -5,11 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
 public class WebSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -21,8 +27,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // 회원 관리 처리 API (POST /user/**) 에 대해 CSRF 무시
-        http.csrf()
-                .ignoringAntMatchers("/user/**");
+        http.csrf().disable();
 
         http
                 .authorizeHttpRequests((authz) -> authz
@@ -35,15 +40,22 @@ public class WebSecurityConfig {
                         // 어떤 요청이든 '인증'
                         .anyRequest().authenticated()
                 )
-                // 로그인 기능 허용
+                // [로그인 기능]
                 .formLogin()
+                // 로그인 View 제공 (GET /user/login)
                 .loginPage("/user/login")
+                // 로그인 처리 (POST /user/login)
+                .loginProcessingUrl("/user/login")
+                // 로그인 처리 후 성공 시 URL
                 .defaultSuccessUrl("/")
+                // 로그인 처리 후 실패 시 URL
                 .failureUrl("/user/login?error")
                 .permitAll()
                 .and()
-                //로그아웃 기능 허용
+                // [로그아웃 기능]
                 .logout()
+                // 로그아웃 처리 URL
+                .logoutUrl("/user/logout")
                 .permitAll();
 
         return http.build();
